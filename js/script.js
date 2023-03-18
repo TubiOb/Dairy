cfe372bfdd120c96714fd490108bf88a7d0dc0d0
TubiOb <tubiobaloluwa@gmail.com>
2023-03-18 10:08:01 +0100

    Okay, I think I'm done. Linked the html pages together, and also with their respective script.js files

--- js/script.js
+++ js/script.js
@@ -783,100 +783,13 @@ bin.addEventListener('click', () => {
 });
 
 
-
-// SIGNUP/SIGNIN
-const SignUp = document.querySelector("#sign");
-const SignIn = document.querySelector("#enter");
-const LoginUsername = document.querySelector("#myname");
-const SignupUsername = document.querySelector("#username");
-const FirstName = document.querySelector("#firstname");
-const LastName = document.querySelector("#lastname");
-const Email = document.querySelector("#mymail");
-const SignUpPass = document.querySelector("#mypassword");
-const SignInPass = document.querySelector("#mypassword1");
-
-const signUpButton = document.querySelector('#signUpButton');
-const signInButton = document.querySelector('#signInButton');
-const Home = document.querySelector('#Home');
-
-const mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-const nameRegex = /^[a-zA-Z]+$/;
-// const usernameRegex = /^[a-zA-Z][\w-]{2,19}$/;
-const Passwrdformat = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/;
-
-// let UserData;
+// window.onload = function() {
+//     if (window.location.href !== "index.html") {
+//         window.location.href = "index.html";
+//     }
+// }
 
 
-SignUp.addEventListener('click', () => {
-    console.log("SHUTUPPPPPP")
-    if (FirstName.value.length > 0 && LastName.value.length > 0 && Email.value.length > 0 && SignupUsername.value.length > 0) {
-        if (SignUpPass.value.length >= 8) {
-            if (FirstName.value.match(nameRegex) || LastName.value.match(nameRegex) || Email.value.match(mailformat) || SignupUsername.value.match(nameRegex) || SignUpPass.value.match(Passwrdformat)) {
-                // UserData = {
-                //     FirstName: FirstName.value,
-                //     LastName: LastName.value,
-                //     Username: SignupUsername.value,
-                //     Mail: Email.value,
-                //     Password: SignUpPass.value,
-                // };
-                // let details = [];
-                // details.push(UserData);
-                // localStorage.setItem(`details_${SignupUsername.value}`, JSON.stringify(details));
-                alert("Hello, " + SignupUsername.value + "!");
-                FirstName.value = "";
-                LastName.value = "";
-                SignupUsername.value = "";
-                Email.value = "";
-                SignUpPass.value = "";
-                Home.classList.remove("right-panel-active");
-                // console.log(details);
-                // StoredDetails = details;
-            } else {
-                alert("Invalid Inputs");
-            }
-        } else {
-            alert("Password must have at least 8 characters, one lowercase letter, one uppercase letter, one number, and one special character")
-        }
-
-    } else {
-        alert("Fields cannot be empty");
-    }
-})
-
-
-
-
-SignIn.addEventListener('click', () => {
-    console.log("SCREEEEAAAAMMMMMM")
-    if (LoginUsername.value.length > 0 && SignInPass.value.length > 0) {
-        console.log(LoginUsername.value);
-        console.log(SignInPass.value);
-        if (LoginUsername.value.match(nameRegex) || SignInPass.value.match(Passwrdformat)) {
-            alert("Welcome back, " + LoginUsername.value + "!");
-            var url = './diary.html';
-            window.open(url, '_self');
-            return;
-        } else {
-            alert("Invalid Username or Password");
-            LoginUsername.value = "";
-            SignInPass.value = "";
-            Home.classList.add("right-panel-active");
-        }
-    } else {
-        alert("Fields cannot be empty");
-    }
-});
-
-
-
-
-
-signUpButton.addEventListener('click', () => {
-    console.log("SIGN UP");
-    Home.classList.add("right-panel-active");
-});
-
-signInButton.addEventListener('click', () => {
-    console.log("SIGN OUT");
-    Home.classList.remove("right-panel-active");
-});
+if (performance.navigation.type === 1) {
+    window.location.href = "index.html";
+}
---
+++ js/script1.js
@@ -0,0 +1,84 @@
+// SIGNUP/SIGNIN
+const SignUp = document.querySelector("#sign");
+const SignIn = document.querySelector("#enter");
+const LoginUsername = document.querySelector("#myname");
+const SignupUsername = document.querySelector("#username");
+const FirstName = document.querySelector("#firstname");
+const LastName = document.querySelector("#lastname");
+const Email = document.querySelector("#mymail");
+const SignUpPass = document.querySelector("#mypassword");
+const SignInPass = document.querySelector("#mypassword1");
+
+const signUpButton = document.querySelector('#signUpButton');
+const signInButton = document.querySelector('#signInButton');
+const Home = document.querySelector('#Home1');
+
+const mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+const nameRegex = /^[a-zA-Z]+$/;
+
+const Passwrdformat = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/;
+
+
+SignUp.addEventListener('click', () => {
+    console.log("SHUTUPPPPPP")
+    if (FirstName.value.length > 0 && LastName.value.length > 0 && Email.value.length > 0 && SignupUsername.value.length > 0) {
+        if (SignUpPass.value.length >= 8) {
+            if (FirstName.value.match(nameRegex) || LastName.value.match(nameRegex) || Email.value.match(mailformat) || SignupUsername.value.match(nameRegex) || SignUpPass.value.match(Passwrdformat)) {
+
+                alert("Hello, " + SignupUsername.value + "!");
+                FirstName.value = "";
+                LastName.value = "";
+                SignupUsername.value = "";
+                Email.value = "";
+                SignUpPass.value = "";
+                Home.classList.remove("right-panel-active");
+
+            } else {
+                alert("Invalid Inputs");
+            }
+        } else {
+            alert("Password must have at least 8 characters, one lowercase letter, one uppercase letter, one number, and one special character")
+        }
+
+    } else {
+        alert("Fields cannot be empty");
+    }
+})
+
+
+
+
+SignIn.addEventListener('click', () => {
+    console.log("SCREEEEAAAAMMMMMM")
+    if (LoginUsername.value.length > 0 && SignInPass.value.length > 0) {
+        console.log(LoginUsername.value);
+        console.log(SignInPass.value);
+        if (LoginUsername.value.match(nameRegex) || SignInPass.value.match(Passwrdformat)) {
+            alert("Welcome back, " + LoginUsername.value + "!");
+            var url = './diary.html';
+            window.open(url, '_self');
+            return;
+        } else {
+            alert("Invalid Username or Password");
+            LoginUsername.value = "";
+            SignInPass.value = "";
+            Home.classList.add("right-panel-active");
+        }
+    } else {
+        alert("Fields cannot be empty");
+    }
+});
+
+
+
+
+
+signUpButton.addEventListener('click', () => {
+    console.log("SIGN UP");
+    Home.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+    console.log("SIGN OUT");
+    Home.classList.remove("right-panel-active");
+});
